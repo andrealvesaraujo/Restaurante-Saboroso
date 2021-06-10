5a03cc5b1d7e2100da6e6ff068b2c7233cd2e85e
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-06-10 12:06:54 -0300

    MY41 - Administração dos usuários alterando senha

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -45,16 +45,18 @@ class HcodeGrid {
         this.options = Object.assign({}, {
             formCreate:'#modal-create form',
             formUpdate:'#modal-update form',
-            btnUpdate:'.btn-update',
-            btnDelete:'.btn-delete',
+            btnUpdate:'btn-update',
+            btnDelete:'btn-delete',
             onUpdateLoad: (form,name,data) =>{
 
                 let input = form.querySelector('[name=' + name + ']');
                 if(input) input.value = data[name];
-                
+
             }
         }, configs);
 
+        this.rows = [...document.querySelectorAll('table tbody tr')];
+        
         this.initForms();
         this.initButtons();
 
@@ -64,25 +66,35 @@ class HcodeGrid {
 
         this.formCreate = document.querySelector(this.options.formCreate);
 
-        this.formCreate.save().then(json=>{
+        this.formCreate.save({
 
-            this.fireEvent('afterFormCreate');
+            success: () =>{
+                
+                this.fireEvent('afterFormCreate');
 
-        }).catch(err=>{
+            },
 
-            this.fireEvent('afterFormCreateError');
-            
+            failure: () => {
+                this.fireEvent('afterFormCreateError');
+            }
+
         });
 
         this.formUpdate = document.querySelector(this.options.formUpdate);
 
-        this.formUpdate.save().then(json=>{
+        this.formUpdate.save({
 
-            this.fireEvent('afterFormUpdate');
+            success: () =>{
+                
+                this.fireEvent('afterFormUpdate');
 
-        }).catch(err=>{
 
-            this.fireEvent('afterFormUpdateError');
+            },
+            
+            failure: () => {
+                this.fireEvent('afterFormUpdateError');
+
+            }
 
         });
 
@@ -107,51 +119,68 @@ class HcodeGrid {
 
     }
 
-    initButtons() {
-    
-        [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn=>{
+    btnUpdateClick(e) {
 
-            btn.addEventListener('click', e => {
-
-                this.fireEvent('beforeUpdateClick', [e]); 
+        this.fireEvent('beforeUpdateClick', [e]); 
                 
-                let data = this.getTrData(e);
+        let data = this.getTrData(e);
 
-                for(let name in data) {
+        for(let name in data) {
 
-                    this.options.onUpdateLoad(this.formUpdate, name, data);                      
+            this.options.onUpdateLoad(this.formUpdate, name, data);                      
 
-                }
+        }
 
-                this.fireEvent('afterUpdateClick', [e]);
+        this.fireEvent('afterUpdateClick', [e]);
 
-            });
-
-        });
+    }
 
 
-        [...document.querySelectorAll(this.options.btnDelete)].forEach(btn=>{
+    btnDeleteClick(e) {
+
+        this.fireEvent('beforeDeleteClick');
+
+        let data = this.getTrData(e);
         
-            btn.addEventListener('click', e => {
-
-                this.fireEvent('beforeDeleteClick');
-
-                let data = this.getTrData(e);
-                
-                if(confirm(eval('`'+ this.options.deleteMsg + '`'))) {
-                    
-                    fetch(eval('`'+ this.options.deleteUrl + '`'), {
-                        method : 'DELETE'
-                        })
-                        .then(response=>response.json())
-                        .then(json=>{
-
-                            this.fireEvent('afterDeleteClick');
-
-                        });
-
-                }    
+        if(confirm(eval('`'+ this.options.deleteMsg + '`'))) {
             
+            fetch(eval('`'+ this.options.deleteUrl + '`'), {
+                method : 'DELETE'
+                })
+                .then(response=>response.json())
+                .then(json=>{
+
+                    this.fireEvent('afterDeleteClick');
+
+                });
+
+        }    
+
+    }
+
+    initButtons() {
+
+        this.rows.forEach(row=>{
+
+            [...row.querySelectorAll('.btn')].forEach(btn => {
+
+                btn.addEventListener('click', e => {
+
+                    if(e.target.classList.contains(this.options.btnUpdate)) {
+
+                        this.btnUpdateClick(e);
+
+                    } else if(e.target.classList.contains(this.options.btnDelete)){
+
+                        this.btnDeleteClick(e);
+
+                    } else {
+
+                        this.fireEvent('buttonClick', [e.target, this.getTrData(e), e]);
+
+                    }
+
+                });
             });
 
         });
